873a4baf1ae37b6a61dc3963387853e3ff0d2af0
HunterXBook <applehunterpro@gmail.com>
2025-05-26 16:23:21 +0300

    сообщение fix 9

--- src/components/pages/ContactPage.tsx
+++ src/components/pages/ContactPage.tsx
@@ -93,6 +93,7 @@ const ContactPage: React.FC = () => {
                 value={formData.message}
                 onChange={handleChange}
                 rows={4}
+                placeholder="Например, укажите название модели кондиционера, которая Вас интересует"
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
               ></textarea>
